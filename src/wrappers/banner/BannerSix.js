b2fc5aedcebdcb6e058f70b583e784d34c5f4da2
JocelinT <joceln.it.teseo@gmail.com>
2024-10-26 22:21:20 -0600

    Descripcion

--- src/wrappers/banner/BannerSix.js
+++ src/wrappers/banner/BannerSix.js
@@ -20,7 +20,7 @@ const BannerSix = ({ spaceTopClass, spaceBottomClass }) => {
               <Link to={process.env.PUBLIC_URL + "/shop-grid-standard"}>
                 <img
                   src={
-                    process.env.PUBLIC_URL + "/assets/img/banner/Kits.webp"
+                    process.env.PUBLIC_URL + "/assets/img/banner/Kits.png"
                   }
                   alt=""
                 />
@@ -38,7 +38,7 @@ const BannerSix = ({ spaceTopClass, spaceBottomClass }) => {
               <Link to={process.env.PUBLIC_URL + "/shop-grid-standard"}>
                 <img
                   src={
-                    process.env.PUBLIC_URL + "/assets/img/banner/Individuales.webp"
+                    process.env.PUBLIC_URL + "/assets/img/banner/Individuales.png"
                   }
                   alt="Cafe Golden"
                 />
